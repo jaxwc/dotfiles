d3255e4c60304c40be3d2dc688324fe156b9d0ff
Jackson Clark <124289919+jaxwc@users.noreply.github.com>
2025-05-15 12:47:29 -0500

    Add files via upload

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            require("tokyonight").setup({
+                style = "storm",          -- Sets the variant to 'storm'
+                transparent = true,       -- Enables transparent background
+                terminal_colors = true,   -- Use terminal colors
+                styles = {
+                    sidebars = "transparent",
+                    floats = "transparent"
+                }
+            })
+            vim.cmd.colorscheme("tokyonight-storm")
+        end
+    },
+    {
+        "catppuccin/nvim",
+        lazy = true,
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+            })
+        end
+    }
+}
