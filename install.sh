eb624803f3548dc2ed6cd93b842b3ebccb354ab3
Jackson Clark <124289919+jaxwc@users.noreply.github.com>
2025-05-19 10:30:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
+##missing a lot 
 
+brew install 1password@beta
+brew install alt-tab
+brew install arc
+brew install cleanshotx
+brew install fantastical
+brew install ghostty
+brew install obsidian
+brew install raycast
+brew install spotify
+brew install visual-studio-code
