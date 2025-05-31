980cd01bf0ccaeb1ecda4e45b30a2e0bde779851
Jackson Clark <124289919+jaxwc@users.noreply.github.com>
2025-05-30 20:32:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
-##missing a lot 
-
-brew install 1password@beta
-brew install alt-tab
 brew install zen-browser
+brew install 1password@beta
+brew install ghostty
 brew install cleanshotx
 brew install fantastical
-brew install ghostty
 brew install obsidian
 brew install raycast
 brew install spotify
 brew install visual-studio-code
+brew install 
+brew install alt-tab
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew install borders
+
+brew install --cask nikitabobko/tap/aerospace
+
+brew tap epk/epk
+brew install font-sf-mono-nerd-font
+
+brew install nvim
+brew install tmux
