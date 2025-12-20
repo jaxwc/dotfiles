aaaf629c13f9a3b1a3085623a6c2fc2783aa6d26
Jackson <jacksonwc@proton.me>
2025-12-20 11:48:37 -0600

    working on install

---
+++ install.sh
@@ -0,0 +1,44 @@
+# tap
+echo "Tapping Brew..."
+brew tap homebrew/cask-fonts
+brew tap FelixKratz/formulae
+brew tap epk/epk
+brew tap nikitabobko/tap
+
+echo "Install Cask..."
+brew install gh
+brew install git
+brew install lua
+brew install luarocks
+brew install neovim
+brew install starship
+brew install stow
+brew install tmux
+brew install zsh-eutosuggestions
+brew install felixkratz/formulae/borders
+brew install felixkratz/formulae/sketchybar
+brew install 1password@beta
+brew install aerospace
+brew install cleanshot
+brew install fantastical
+brew install ghostty@tip
+brew install obsidian
+brew install protonvpn
+brew install raycast
+brew install spotify
+brew install zen
+brew install python@3.14
+
+echo "Installing Fonts..."
+brew install font-jetbrains-mono-nerd-font
+brew install font-sf-mono-nerd-font
+brew install font-sf-pro
+brew install font-sketchybar-app-font
+brew install sf-symbols
+
+# random
+brew install ghostscript
+brew install imagemagick
+brew install nowplaying-cli
+brew install switchaudio-osx
+
